75643a7a1e126f6139c6d554277138cebb7086ff
Oscar Fernandez Jardi <oscar.fernandez.jardi.st@everis.com>
2020-11-27 17:37:42 +0100

    project_ini_branch Project Init

---
+++ MovieApp/MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Oscar Fernandez Jardi on 27/11/2020.
+//  Copyright © 2020 Oscar Fernandez Jardi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
